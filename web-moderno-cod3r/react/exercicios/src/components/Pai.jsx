a5bb8b5c214d5d01611a931cd27103f98e442a5e
Samuel Simões <samuelsimoes31@gmail.com>
2020-06-25 14:17:26 -0300

    Componentes Pai e Filho #02

--- web-moderno-cod3r/react/exercicios/src/components/Pai.jsx
+++ web-moderno-cod3r/react/exercicios/src/components/Pai.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
-import Filho from './Filho'
+
+function childrenWithProps(props) {
+    return React.Children.map(props.children, child => {
+        return React.cloneElement(child,{
+            ...props, ...child.props
+        })
+    })
+}
 
 export default props => 
     <div>
         <h1>{props.nome} {props.sobrenome}</h1>
         <h2>Filhos</h2>
         <ul>
-            <Filho nome="Pedro" sobrenome={props.sobrenome}/>
-            <Filho {...props}/>
-            <Filho nome="Carla1" {...props} />
-            <Filho {...props} nome="Carla2"/>
+            {props.children}
+            <hr/>
+            {
+                childrenWithProps(props)
+            }
         </ul>
     </div>
--- web-moderno-cod3r/react/exercicios/src/index.js
+++ web-moderno-cod3r/react/exercicios/src/index.js
@@ -8,11 +8,16 @@ import {BoaTarde, BoaNoite} from './components/Multiplos'
 import Saudacao from './components/Saudacao'
 
 import Pai from './components/Pai'
+import Filho from './components/Filho'
 
 // const elemento = <h1>React 2</h1>
 
 ReactDOM.render(
     <div>
-        <Pai nome="Paulo" sobrenome="Silva"/>
+        <Pai nome="Paulo" sobrenome="Silva">
+            <Filho nome="Pedro" />
+            <Filho nome="Paulo" />
+            <Filho nome="Carla" />
+        </Pai>
     </div>
 ,document.getElementById('root'))
